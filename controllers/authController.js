a65111cbb7bdc87134e9c6ff5064b534c116e1b3
jopakka <jopakkamies@gmail.com>
2020-11-17 15:12:27 +0200

    production/ localhost codes

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,43 +3,48 @@ const {validationResult} = require('express-validator');
 const bcrypt = require('bcryptjs');
 
 const signup = async (req, res, next) => {
-  console.log('signup', req.body);
-  // TODO: Register new user, hash password
-  const errors = validationResult(req);
+  //console.log('authController signup', req.body);
 
+  const errors = validationResult(req);
   // If there is errors in login parameters, then return errors
   // else continue
   if (!errors.isEmpty()) {
     return res.send(errors.array());
   }
 
-  const salt = bcrypt.genSaltSync(10)
-  const hash = bcrypt.hashSync(req.body.password, salt)
+  // Hashes password
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(req.body.password, salt);
 
   const params = [
-      req.body.username,
-      req.body.email,
-      hash
-  ]
+    req.body.username,
+    req.body.email,
+    hash,
+  ];
+
+  //console.log('authController params', params);
 
   // TODO: SQL query
-  //const query = await userModel.addUser(params)
+  //const query = await userModel.addUser(params);
 
-  if(/* query */ true) {
+  // If query return no errors then continue
+  // else send error message
+  if (/* query */ true) {
     next();
   } else {
-    res.status(400).json({error: 'Add some useful error message here please'})
+    res.status(400).json({error: 'Add some useful error message here please'});
   }
 };
 
 const login = (req, res) => {
+  console.log('authController login', req.body);
   // TODO: login using passport authenticator
 
 };
 
 const logout = (req, res) => {
-  req.logout()
-  res.json({message: 'Logged out successfully'})
+  req.logout();
+  res.json({message: 'Logged out successfully'});
 };
 
 module.exports = {
---
+++ production.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = (app, port) => {
+  // TODO: Redirect to https
+
+  app.listen(port, () => console.log(`Production app is listening port ${port}`));
+}
