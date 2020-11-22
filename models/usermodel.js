031b8a2c8c3b24821b36e2e8e81b30f7d41c1127
jopakka <jopakkamies@gmail.com>
2020-11-22 22:42:13 +0200

    fixed query

--- models/usermodel.js
+++ models/usermodel.js
@@ -41,11 +41,12 @@ const getUserLogin = async (email) => {
 // TODO: Add correct information to user table
 const addUser = async (params) => {
   try {
+    const user = await connection.execute('SELECT * FROM User WHERE email = ?', params[1])
+    console.log('user', user)
+
     const [status] = await connection.execute(
         'INSERT INTO User(username, email, passwd) VALUES(?,?,?)', params,
     );
-
-    console.log('addUser status', status)
 
     return await getUser(status['insertId']);
   } catch (e) {
