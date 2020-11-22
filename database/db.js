e9cfc9b74a5ffa083c674f6c6f60c607f8e78d86
jopakka <jopakkamies@gmail.com>
2020-11-22 21:44:13 +0200

    db connection try catch

--- database/db.js
+++ database/db.js
@@ -1,11 +1,15 @@
 'use strict';
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+try {
+  module.exports = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+} catch (e) {
+  console.error('db connection error', e.message);
+}
 
-module.exports = connection;
+
