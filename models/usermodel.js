8c0ea490e8d2b2913cdecb6369025097be81fc1c
jopakka <jopakkamies@gmail.com>
2020-11-22 23:15:36 +0200

    fixed query

--- models/usermodel.js
+++ models/usermodel.js
@@ -54,6 +54,7 @@ const addUser = async (params) => {
 
     return await getUser(status['insertId']);
   } catch (e) {
+    console.error('addUser error', e)
     return errorJson(e.message);
   }
 };
