fcc91087ff55850d544b89533c1384b048fae2a4
jopakka <jopakkamies@gmail.com>
2020-11-22 23:13:35 +0200

    fixed query

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 'use strict';
 const mysql = require('mysql2');
 
-const connection = await mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
