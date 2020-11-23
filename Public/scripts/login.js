fe7f08de39cb9b3cd79be0db042c890befa9d190
Michael Carter <michael_carter_21@hotmail.com>
2020-11-24 01:31:49 +0200

    logging in with test user and using fetch to get their username displayed on homepage

--- Public/scripts/login.js
+++ Public/scripts/login.js
@@ -1,9 +1,33 @@
 "use strict";
 
 const authenticateLogin = document.getElementById("js-authenticate-login");
+const loginForm = document.getElementById("login-form");
 
-const toggleHide = () => {
-  window.open(`${url}` + "userProfile.html", "_self", false);
+const toggleHide = async (event) => {
+  event.preventDefault();
+  const data = serializeJson(loginForm);
+  const fetchOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  const response = await fetch(url + "auth/login", fetchOptions);
+  const json = await response.json();
+  console.log("login response", json);
+  if (!json.user) {
+    alert(json.message);
+  } else {
+    // save token
+    sessionStorage.setItem("token", json.token);
+    sessionStorage.setItem("user", json.user.email);
+    console.log(sessionStorage.getItem("token"));
+    console.log(json.user.email);
+    //open user profile
+    window.open(`${url}` + "userProfile.html", "_self", false);
+  }
 };
 
 authenticateLogin.addEventListener("click", toggleHide);
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,20 +1,26 @@
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
 const router = express.Router();
-const {body} = require('express-validator');
-const authController = require('../controllers/authController');
+const { body } = require("express-validator");
+const authController = require("../controllers/authController");
 
-router.route('/login').post(authController.login);
-router.route('/logout').post(authController.logout);
-router.route('/register').post([
-      body('username', 'Min 3 characters, max 30 characters').trim().isLength({min: 3, max: 30}),
-      body('email', 'Email is not valid').trim().isEmail(),
-      body('password',
-          `Password must match following requirements: 1 uppercase letter, minimum of 8 characters`).
-          matches('(?=.*[A-Z]).{8,}'),
+router.route("/login").post(authController.login);
+router.route("/logout").post(authController.logout);
+router
+  .route("/register")
+  .post(
+    [
+      body("username", "Min 3 characters, max 30 characters").trim().isLength({ min: 3, max: 30 }),
+      body("email", "Email is not valid").trim().isEmail(),
+      body(
+        "password",
+        `Password must match following requirements: 1 uppercase letter, minimum of 8 characters`
+      ).matches("(?=.*[A-Z]).{8,}"),
     ],
     authController.register,
-    authController.login,
-);
+    authController.login
+  );
+
+router.get(`/user`, authController.user_get);
 
 module.exports = router;
