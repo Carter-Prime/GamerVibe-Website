8aa0f201417ee8bc7ceaab310034b916f6034cdb
Michael Carter <michael_carter_21@hotmail.com>
2020-11-23 23:36:53 +0200

    renamed frontend to Public

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const authRoute = require('./routes/authRoute');
-const postRoute = require('./routes/postRoute');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const authRoute = require("./routes/authRoute");
+const postRoute = require("./routes/postRoute");
 const app = express();
-const passport = require('./utils/pass');
+const passport = require("./utils/pass");
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-if (process.env.NODE_ENV === 'production') {
-  require('./production')(app, process.env.PORT);
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+if (process.env.NODE_ENV === "production") {
+  require("./production")(app, process.env.PORT);
 } else {
-  require('./localhost')(app, process.env.PORT);
+  require("./localhost")(app, process.env.PORT);
 }
 
 // Routes
-app.use(express.static('./frontend'));
-app.use('/auth', authRoute);
-app.use('/post', passport.authenticate('jwt', {session: false}), postRoute);
+app.use(express.static("./Public"));
+app.use("/auth", authRoute);
+app.use("/post", passport.authenticate("jwt", { session: false }), postRoute);
