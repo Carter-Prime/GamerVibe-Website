d3786b4950796a1a913979a3ce2aded926831ff8
jopakka <jopakkamies@gmail.com>
2020-11-24 12:44:53 +0200

    comments

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,7 +4,7 @@ const {validationResult} = require('express-validator');
 const {errorJson} = require('../utils/json_messages');
 const userModel = require('../models/usermodel');
 const resize = require('../utils/resize');
-const fs = require('fs')
+const fs = require('fs');
 
 const new_post = async (req, res) => {
   // console.log('postController new_post body', req.body);
@@ -21,37 +21,47 @@ const new_post = async (req, res) => {
     return res.status(400).json(errorJson(valRes['errors']));
   }
 
+  // Check if user exists
   const user = await userModel.getUser(req.body.userid);
   // console.log('user', user);
   if (user['error']) {
+    // User not exists
     return res.status(400).json(errorJson('Something went wrong'));
   }
 
+  // Add new post to database
   const query = await postModel.add_new_post(req.body.userid, req.body.caption,
       req.file.filename);
 
+  // If query return error
   if (query['error']) {
     return res.status(400).json(query['error']);
   }
 
+  // Makes thumbnail
   const thumb = await make_thumbnail(req);
+  // If thumbnail return error
   if (thumb['error']) {
     return res.status(400).json(thumb);
   }
+
+  // Everything went fine
   res.json(query['insertId']);
 };
 
 const make_thumbnail = async (req) => {
   try {
+    // Creates thumbnails directory if it not exists
     // https://stackoverflow.com/a/26815894
     const dir = './thumbnails';
-    if (!fs.existsSync(dir)){
+    if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
 
     return await resize.makeThumbnail(req.file.path, 300,
         `./thumbnails/${req.file.filename}`);
   } catch (e) {
+    // console.error('postController make_thumbnail error', e.message);
     return errorJson(e.message);
   }
 };
