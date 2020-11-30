fbd4d8dbb4ab07bc92bb16d378abc25899b65c8d
jopakka <jopakkamies@gmail.com>
2020-11-30 11:06:11 +0200

    uncomments

--- controllers/followController.js
+++ controllers/followController.js
@@ -3,7 +3,7 @@ const followingModel = require('../models/followModel');
 const userModel = require('../models/userModel');
 
 const getFollowing = async (req, res) => {
-  console.log('followController getFollowing user', req.user)
+  // console.log('followController getFollowing user', req.user)
   const query = await followingModel.get_following(req.user.user_id);
   // console.log('followController getFollowing query', query);
 
@@ -21,9 +21,9 @@ const getFollowing = async (req, res) => {
 };
 
 const getFollowers = async (req, res) => {
-  console.log('followController getFollowers user', req.user)
+  // console.log('followController getFollowers user', req.user)
   const query = await followingModel.get_followers(req.user.user_id);
-  console.log('followController getFollowers query', query);
+  // console.log('followController getFollowers query', query);
 
   if (query['error']) {
     return res.status(400).json(query);
