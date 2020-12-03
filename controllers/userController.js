295b0fe997aaeb4a13af7e269179515c96467d47
jopakka <jopakkamies@gmail.com>
2020-12-03 23:15:14 +0200

    get user follow check

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,6 @@ const {delete_file, make_thumbnail} = require('../utils/my_random_stuff');
 
 // Get single user
 const getUser = async (req, res) => {
-  // TODO: check if user follows wantedUser or user is moderator
   const user = req.user;
   // console.log('userController getUser req.user', user);
   // console.log('userController getUser params', req.params.id)
