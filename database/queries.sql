0e609b0b4a4d8ff04cfb14821e8bbe8546460420
unknown <Patricie.Suppala@gmail.com>
2020-12-05 09:36:15 +0200

    new queries

--- database/queries.sql
+++ database/queries.sql
@@ -257,16 +257,17 @@ WHERE (tag LIKE '%$tagname%')
 
 -- For discover page
 -- Doesn't show posts from users that user is blocked or from users that accounts are private
+-- !!!!I tested this nd this IMHO does not exclude deleted users
 SELECT p.post_id, p.user_id, u.username, p.caption, p.created_at, p.imgfilename
 FROM Post AS p
 LEFT JOIN User AS u
 ON u.user_id = p.user_id
 LEFT JOIN Blocking AS b
-ON b.blocker_id = 7
+ON b.blocker_id = 3
 AND b.blocking_id = p.user_id
 WHERE p.deleted_at IS NULL
 AND p.banned_at IS NULL
-AND u.user_id != 7
+AND u.user_id != 3
 AND u.private_acc != 1
 AND u.banned_at IS NULL
 AND b.blocked_at IS NULL
@@ -293,3 +294,110 @@ AND (
 AND p.post_id < 999999999
 ORDER BY created_at DESC
 LIMIT 30
+
+-- Discovery query that will not show friends and excludes posts from blocked users and deleted users--
+-- example below for user 3
+SELECT DISTINCT p.post_id,
+    p.user_id,
+    p.caption,
+    u.username,
+    p.created_at,
+    p.imgfilename,
+    p.deleted_at,
+    p.banned_at,
+    (
+        SELECT count(post_id)
+        FROM Upvote l
+        WHERE p.post_id = l.post_id
+            AND l.unliked_at IS NULL
+    ) Upvotes,
+    (
+        SELECT count(approved)
+        FROM Following f
+        WHERE f.following_id = u.user_id
+            AND f.approved = 1
+    ) PosterFollowers,
+    (
+        SELECT count(blocked_at)
+        FROM Blocking AS b
+        WHERE b.blocking_id = u.user_id
+    ) HiddenFrom
+FROM Post AS p,
+    User AS u,
+    Following AS f,
+    Blocking AS b
+WHERE p.deleted_at IS NULL
+    AND p.banned_at IS NULL
+    AND u.user_id != 3
+    AND u.user_id = p.user_id
+    AND (
+        u.private_acc = 0
+    )
+    AND u.deleted_at IS NULL
+    AND u.banned_at IS NULL
+    AND TIMESTAMPDIFF(SECOND, p.created_at, NOW()) > 0
+    AND NOT (
+        b.blocker_id = 3
+        AND b.blocking_id = u.user_id
+        AND b.unblocked_at IS NULL
+    )
+ORDER BY created_at DESC
+LIMIT 30;
+
+
+-- Friends query that will show friends only and excludes posts from blocked users and deleted users and itself--
+-- example below for user 3
+SELECT DISTINCT p.post_id,
+    p.user_id,
+    p.caption,
+    u.username,
+    p.created_at,
+    p.imgfilename,
+    p.deleted_at,
+    p.banned_at,
+    (
+        SELECT count(post_id)
+        FROM Upvote l
+        WHERE p.post_id = l.post_id
+            AND l.unliked_at IS NULL
+    ) Upvotes,
+    (
+        SELECT count(approved)
+        FROM Following f
+        WHERE f.following_id = u.user_id
+            AND f.approved = 1
+    ) PosterFollowers,
+    (
+        SELECT count(blocked_at)
+        FROM Blocking AS b
+        WHERE b.blocking_id = u.user_id
+    ) HiddenFrom
+FROM Post AS p,
+    User AS u,
+    Following AS f,
+    Blocking AS b
+WHERE p.deleted_at IS NULL
+    AND p.banned_at IS NULL
+    AND u.user_id = p.user_id
+     AND u.user_id != 3
+    AND (
+            f.follower_id = 3
+            AND f.following_id = u.user_id
+            AND f.approved = 1
+            AND (u.private_acc = 0 OR u.private_acc = 1)
+        )
+    
+    AND u.deleted_at IS NULL
+    AND u.banned_at IS NULL
+    AND TIMESTAMPDIFF(SECOND, p.created_at, NOW()) > 0
+    AND NOT (
+        b.blocker_id = 3
+        AND b.blocking_id = u.user_id
+        AND b.unblocked_at IS NULL
+    )
+ORDER BY created_at DESC
+LIMIT 30;
+
+-- Query for own posts only
+
+-- Query of users a user is blocking
